dbf7b4152e5179f34bad2e1c1e7e6f4397037731
Water <hongsheng322@gmail.com>
2018-09-07 17:13:49 +0800

    Point and Click
    raycast point & click. shows the name of the game object when u tap it

---
+++ Vuforia AR Outreach/Assets/Scripts/TouchController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TouchController : MonoBehaviour {
+    //string btnName;
+    public Text object_text;
+
+	// Use this for initialization
+	void Start () {
+        object_text.text = "HELLO";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            RaycastHit Hit;
+            if(Physics.Raycast(ray, out Hit))
+            {
+                //btnName = Hit.transform.name;
+                object_text.text = Hit.collider.name;
+            }
+        }
+	}
+}
